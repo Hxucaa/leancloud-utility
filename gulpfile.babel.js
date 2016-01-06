ec90d6dfdbf3e54c2c557e43655801998bb196d6
Lance Zhu <lancezhu77@gmail.com>
2016-01-06 12:42:06 -0800

    Use gulp to manage all scripts.
    Summary: Centralize scripts
    
    Test Plan: Tested
    
    Reviewers: cwang, wqi
    
    Subscribers: jbot
    
    Differential Revision: https://dev.lai.io/D114

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -75,12 +75,48 @@ gulp.task("test", callback => {
   );
 });
 
-gulp.task("test:w", ["test:unit", "test:gen"], () => {
+gulp.task("test:w", ["test"], () => {
   gulp.watch(
     ["src/**", "test/**"],
-    ["test:unit", "test:gen"]
+    ["test"]
   );
 });
+
+gulp.task("coverage:unit", ["clean:coverage:unit"], () => {
+  return gulp
+    .src(["README.md"], { read: false })
+    .pipe(shell(["NODE_ENV=test node_modules/.bin/babel-node node_modules/.bin/babel-istanbul cover --dir coverage/unit-test node_modules/.bin/_mocha -- test/unit --opts test/mocha.opts --harmony"]));  // eslint-disable-line max-len
+});
+
+gulp.task("coverage:unit:w", ["coverage:unit"], () => {
+  gulp.watch(lintSrc, ["coverage:unit"]);
+});
+
+gulp.task("coverage:unit:ci", ["clean:coverage:unit"], () => {
+  return gulp
+    .src(["README.md"], { read: false })
+    .pipe(shell(["NODE_ENV=ci node_modules/.bin/babel-node node_modules/.bin/babel-istanbul cover --dir coverage/unit-test node_modules/.bin/_mocha -- -R dot test/unit --opts test/mocha.opts --harmony"]));  // eslint-disable-line max-len
+});
+
+gulp.task("coverage:gen", ["clean:coverage:gen"], () => {
+  return gulp
+    .src(["README.md"], { read: false })
+    .pipe(shell(["NODE_ENV=test node_modules/.bin/babel-node node_modules/.bin/babel-istanbul cover --dir coverage/generative-test node_modules/.bin/_mocha -- test/generative --opts test/mocha.opts --harmony"]));  // eslint-disable-line max-len
+});
+
+gulp.task("coverage:gen:w", ["coverage:gen"], () => {
+  gulp.watch(lintSrc, ["coverage:unit"]);
+});
+
+gulp.task("coverage:gen:ci", ["clean:coverage:gen"], () => {
+  return gulp
+    .src(["README.md"], { read: false })
+    .pipe(shell(["NODE_ENV=ci node_modules/.bin/babel-node node_modules/.bin/babel-istanbul cover --dir coverage/generative-test node_modules/.bin/_mocha -- -R dot test/generative --opts test/mocha.opts --harmony"]));  // eslint-disable-line max-len
+});
+
+
+gulp.task("coverage", ["coverage:unit", "coverage:gen"]);
+gulp.task("coverage:ci", ["coverage:unit:ci", "coverage:gen:ci"]);
 
 gulp.task("lint", () => {
   // ESLint ignores files with "node_modules" paths.
@@ -118,7 +154,7 @@ gulp.task("jsdoc:w", ["jsdoc"], () => {
   gulp.watch(["src/**"], ["jsdoc"]);
 });
 
-gulp.task("dist:compile", ["clean:dist"], () => {
+gulp.task("build", ["clean:build"], () => {
   return gulp
     .src(["src/**"], { base: "./src" })
     .pipe(babel({
@@ -131,9 +167,21 @@ gulp.task("dist:compile:w", ["dist:compile"], () => {
   gulp.watch(["src/**"]);
 });
 
-gulp.task("clean:dist", () => {
+gulp.task("clean:build", () => {
   return del([
     "dist/**"
+  ]);
+});
+
+gulp.task("clean:coverage:unit", () => {
+  return del([
+    "coverage/unit-test/**"
+  ]);
+});
+
+gulp.task("clean:coverage:gen", () => {
+  return del([
+    "coverage/generative-test/**"
   ]);
 });
 
